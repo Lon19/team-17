5ed68da5b0b2e4c58bfb88ceb875087294856cdf
Samuel Gostling <44071408+SamGostling@users.noreply.github.com>
2019-11-02 01:41:01 +0000

    #ItFinallyFreakingWorks

--- meterChange.cs
+++ meterChange.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 
 public class meterChange : MonoBehaviour
 {
+    private Image greenArea;
     private Slider slider;
     private int speed = 1;
     private bool increasing = true;
     private float powerBar = 0;
+    RectTransform greenAreaRectTransform;
     // Start is called befores the first frame update
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
+        greenArea = slider.GetComponentsInChildren<Image>()[2];
+        greenAreaRectTransform = greenArea.transform as RectTransform;
     }
 
     // Update is called once per frame
@@ -35,16 +39,21 @@ public class meterChange : MonoBehaviour
         }
     }
 
-    bool checkIfBallIsHit()
+    bool checkIfBallIsHit(float difficulty, int speed)
     {
-        if (slider.value >0.4 && slider.value < 0.6)
+        //changes the scale of the green bar based on float received
+        greenAreaRectTransform.localScale = new Vector3(difficulty, 1, 1);
+        changeSpeed(speed);
+
+        //calculates the true value zone
+        if (slider.value >= (0+difficulty) && slider.value <= (1 - difficulty))
         {
             return true;
         }
         return false;
     }
 
-    void change(int speed)
+    private void changeSpeed(int speed)
     {
         this.speed = speed;
     }
